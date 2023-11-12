a18f09488e761690370c5dc51197661ac4c40959
unknown <rivaldogabriel531@gmail.com>
2023-11-12 15:28:46 +0700

    UpdateAdminPage

--- src/pages/admin/Report.jsx
+++ src/pages/admin/Report.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import {Table } from "react-bootstrap";
+const Report = () => {
 
-function Report() {
+
   return (
-    <div>Report</div>
-  )
-}
+    <Table striped bordered hover responsive className="container">
+      <thead>
+        <tr>
+          <th className="col-2 text-center">Image</th>
+          <th className="col-3 text-center">Product Name</th>
+          <th className="col-2 text-center">Price</th>
+          <th className="col-1 text-center">Sold</th>
+        </tr>
+      </thead>
+      <tbody>
+              <tr>
+                <td className="d-flex align-items-center justify-content-center">
+                
+                </td>
+                <td className="text-center align-middle">{""}</td>
+                <td className="text-center align-middle">
+                  
+                </td>
+                <td className="text-center align-middle">
+                  
+                </td>
+              </tr>
+              
+              
+            <article>
+              <hr />
+              <h4>Total Earning :</h4>
+            </article>
 
-export default Report
+      
+      </tbody>
+    </Table>
+  );
+  
+};
+
+
+
+export default Report;
